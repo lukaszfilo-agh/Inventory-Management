36aa5ff2486d5117b3737a864f9ff26c534162bf
Łukasz <lukaszfilo@student.agh.edu.pl>
2025-03-10 21:19:09 +0100

    added delete

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
-from typing import Annotated
+from typing import Annotated, List
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from database import SessionLocal, engine
@@ -58,20 +58,37 @@ async def create_warehouse(warehouse: WarehouseBase, db: db_dependency):
     db.refresh(new_warehouse)
     return new_warehouse
 
-@app.get('/warehouses/', response_model=list[WarehouseModel])
+@app.get('/warehouses/', response_model=List[WarehouseModel])
 async def get_warehouses(db: db_dependency):
     warehouses = db.query(models.Warehouse).all()
     return warehouses
 
 @app.post('/items/', response_model=ItemModel)
 async def create_item(item: ItemBase, db: db_dependency):
+    warehouse = db.query(models.Warehouse).filter(models.Warehouse.id == item.warehouse_id).one_or_none()
+    
+    if not warehouse:
+        raise HTTPException(status_code=404, detail=f"Warehouse with ID {item.warehouse_id} does not exist.")
+    
     new_item = models.Item(**item.model_dump())
     db.add(new_item)
     db.commit()
     db.refresh(new_item)
     return new_item
 
-@app.get('/items/', response_model=list[ItemModel])
+@app.get('/items/', response_model=List[ItemModel])
 async def get_items(db: db_dependency):
     items = db.query(models.Item).all()
     return items
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(models.Item).filter(models.Item.id == item_id).one_or_none()
+    
+    if not item:
+        raise HTTPException(status_code=404, detail=f"Item with ID {item_id} does not exist.")
+    
+    db.delete(item)
+    db.commit()
+    
+    return {"message": f"Item with ID {item_id} has been deleted successfully."}

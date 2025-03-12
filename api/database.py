63c56091d06cfdc4197906ba76bd9dde22ea5e12
Łukasz <lukaszfilo@student.agh.edu.pl>
2025-03-12 14:23:05 +0100

    added better env support

--- api/database.py
+++ api/database.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
+
+if os.getenv('RUNNING_IN_DOCKER') != 'true':
+    load_dotenv()
 
 URL_DB = 'sqlite:///./db/inventory.db'
 

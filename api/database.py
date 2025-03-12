b23976ec4ed6d576b05be304562f5c32355f7a92
Łukasz <lukaszfilo@student.agh.edu.pl>
2025-03-12 09:50:02 +0100

    part 2 of db

--- api/database.py
+++ api/database.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
 
 URL_DB = 'sqlite:///./db/inventory.db'
 
-engine = create_engine(URL_DB, connect_args={'check_same_thread': False})
+DATABASE_URL = os.getenv("DATABASE_URL", URL_DB)
+
+# engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
